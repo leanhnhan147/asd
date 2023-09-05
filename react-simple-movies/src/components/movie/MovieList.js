3ecda94e028ea036636c27c8940694ddb5a113c1
leanhnhan147 <anhnhan9a3@gmail.com>
2023-09-05 20:24:36 +0700

    fix load movie cart from API

--- react-simple-movies/src/components/movie/MovieList.js
+++ react-simple-movies/src/components/movie/MovieList.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SwiperSlide, Swiper } from "swiper/react";
 import MovieCard from "./MovieCard";
 import "swiper/scss";
+import useSWR from "swr";
+import { fetcher } from "../../Config";
 
 const MovieList = () => {
+  const [movies, setMovies] = useState([]);
+  const { data, error } = useSWR(
+    "https://api.themoviedb.org/3/movie/now_playing?api_key=798ec05a67fae3f0f02ccab8abbc178f",
+    fetcher
+  );
+  useEffect(() => {
+    if (data && data.results) {
+      setMovies(data.results);
+    }
+  }, [data]);
+  console.log(data);
+
   return (
     <div className="movie-list">
       <Swiper grabCursor={"true"} spaceBetween={40} slidesPerView={"auto"}>
